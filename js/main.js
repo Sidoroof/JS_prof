e8890066ba47135b9bb42668b8646b86a9b4d097
SidorovAA <Sidorovsansanih@gmail.com>
2022-08-19 17:49:59 +0300

    ccorrect projet lesson 1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 25000, img: 'img/notebook.jpg'},
-    {id: 2, title: 'Mouse', price: 1220, img: 'img/mous.jpg'},
-    {id: 3, title: 'Keyboard', price: 2200, img: 'img/keyboard.jpg'},
-    {id: 4, title: 'Gamepad', price: 5000, img: 'img/gamepad.jpg'},
-    {id: 5, title: 'Headphones', price: 2550, img: 'img/headphones.jpg'},
-    {id: 5, title: 'Microphone', price: 650, img: 'img/microphone.jpg'},
-    {id: 5, title: 'Telephone', price: 7500, img: 'img/telephone.jpg'},
-    {id: 5, title: 'Switch', price: 10000, img: 'img/switch.jpg'},
+    { id: 1, title: 'Notebook', price: 25000, img: 'img/notebook.jpg' },
+    { id: 2, title: 'Mouse', price: 1220, img: 'img/mous.jpg' },
+    { id: 3, title: 'Keyboard', price: 2200, img: 'img/keyboard.jpg' },
+    { id: 4, title: 'Gamepad', price: 5000, img: 'img/gamepad.jpg' },
+    { id: 5, title: 'Headphones', price: 2550, img: 'img/headphones.jpg' },
+    { id: 5, title: 'Microphone', price: 650, img: 'img/microphone.jpg' },
+    { id: 5, title: 'Telephone', price: 7500, img: 'img/telephone.jpg' },
+    { id: 5, title: 'Switch', price: 10000, img: 'img/switch.jpg' },
 ];
-
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
 const renderProduct = (obj) => {
     return `<div class="product-item">
-                <div class="cart-item"> 
-                    <img class="img-style" src="${obj.img}" alt=”${obj.title}”>           
+                <div class="cart-item">
+                    <img class="img-style" src="${obj.img}" alt=”${obj.title}”>
                     <h3 class ="h-style"> ${obj.title}</h3>
-                    <p class ="p-style"> ${obj.price}</p>                    
+                    <p class ="p-style"> ${obj.price}</p>
                     <button class="buy-btn">Купить</button>
                 </div>
             </div>`
@@ -26,5 +25,4 @@ const renderPage = list => {
     console.log(productsList);
     document.querySelector('.products').innerHTML = productsList.join("");
 };
-
 renderPage(products);
